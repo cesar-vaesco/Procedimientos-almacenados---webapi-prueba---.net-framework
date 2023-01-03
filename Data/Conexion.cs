c543cd874d04c11298135ec642aec4ea7b26f4df
Cesar-Dev <cesarvargasescorcia@gmail.com>
2023-01-02 19:32:35 -0600

    Procedimientos almacenados - webapi prueba - .net framework

---
+++ Data/Conexion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPIPrueba.Data
+{
+    public class Conexion
+    {
+        public static string rutaConexion = "Data Source=.; Initial Catalog=DBPRUEBAS; Integrated Security=True";
+        //public static string rutaConexion = "Data Source=VAESCODE; Initial Catalog=DBPRUEBAS; User ID=Ces_dev; pass=pass; ";
+
+    }
+}
---
+++ Models/Usuario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace WebAPIPrueba.Models
+{
+    public class Usuario
+    {
+        public int IdUsuario { get; set; }  
+        public string DocumentoIdentidad { get; set; }
+        public string Nombre { get; set; }
+        public string Telefono { get; set; }
+        public string Correo { get; set; }
+        public string Ciudad { get; set; }
+        public DateTime FechaRegistro { get; set; }
+
+    }
+}
